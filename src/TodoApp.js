862b302b04c8347cc247bc9631c5646de2dfae02
Megan <mvyenielo58@gmail.com>
2023-09-28 16:30:41 -0700

    started TodoApp component

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -3,6 +3,7 @@ import { v4 as uuid } from "uuid";
 
 import TopTodo from "./TopTodo";
 import EditableTodoList from "./EditableTodoList";
+import TodoForm from "./TodoForm";
 
 /** App for managing a todo list.
  *
@@ -16,43 +17,49 @@ import EditableTodoList from "./EditableTodoList";
  */
 
 function TodoApp() {
+  const [todos, setTodos] = useState([]);
 
   /** add a new todo to list */
   function create(newTodo) {
+    setTodos(todos => [...todos, newTodo]);
   }
 
   /** update a todo with updatedTodo */
   function update(updatedTodo) {
+    setTodos(todos.map(todo => todo.id === updatedTodo.id ? updatedTodo : todo));
   }
 
   /** delete a todo by id */
   function remove(id) {
+    setTodos(todos => todos.filter(todo => todo.id !== id));
   }
 
   return (
-      <main className="TodoApp">
-        <div className="row">
+    <main className="TodoApp">
+      <div className="row">
 
-          <div className="col-md-6">
-            <EditableTodoList /> OR
+        <div className="col-md-6">
+          {todos.length > 0 ?
+            <EditableTodoList /> :
             <span className="text-muted">You have no todos.</span>
-          </div>
+          }
+        </div>
 
-          <div className="col-md-6">
-            (if no top todo, omit this whole section)
+        <div className="col-md-6">
+          {todos.length > 0 &&
             <section className="mb-4">
               <h3>Top Todo</h3>
               <TopTodo />
-            </section>
+            </section>}
 
-            <section>
-              <h3 className="mb-3">Add Nü</h3>
-              FIXME
-            </section>
-          </div>
-
+          <section>
+            <h3 className="mb-3">Add Nü</h3>
+            <TodoForm />
+          </section>
         </div>
-      </main>
+
+      </div>
+    </main>
   );
 }
 
